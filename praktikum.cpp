81a9b40f99d28be38b11cf85c73067d90aef3ebb
Rizhuycihuy <rizki003131@gmail.com>
2026-03-03 16:10:34 +0700

    perubahan 4

--- praktikum.cpp
+++ praktikum.cpp
@@ -4,7 +4,7 @@ using namespace std;
 float p,l;
 
 void input(){
-  cout << "masukkan panjanggg : " ;
+  cout << "masukkan panjangggg : " ;
   cin >> p;
   cout << "masukkan lebar : " ;
   cin >> l;
