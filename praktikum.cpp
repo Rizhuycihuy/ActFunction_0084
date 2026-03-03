ae890427b6e5e66d88bd584b0b4dbfb17423d99b
Rizhuycihuy <rizki003131@gmail.com>
2026-03-03 16:00:53 +0700

    first commit

---
+++ praktikum.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+float p,l;
+
+void input(){
+  cout << "masukkan panjang : " ;
+  cin >> p;
+  cout << "masukkan lebar : " ;
+  cin >> l;
+}
+
+float luaspersegi(){
+  return p*l;
+
+}
+
+void output (){
+  cout <<  "hasil nya = " << luaspersegi();
+
+
+
+}
+
+int main()
+{
+  input();
+  output();
+}
